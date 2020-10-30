848a3d1f976a01e3b72e4cf640ecfd26210b191b
Anand Vaidya <anand1711@gmail.com>
2020-10-30 14:30:52 +0530

    new buildout stubs

---
+++ env.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# When you create a cluster on mongo cloud you will get a command to register like below
+# mongo "mongodb+srv://cluster0.ufsyo.mongodb.net/test" --username admin
+# You need to frame the url from there for connection without prompt. The url above becomes
+# mongo "mongodb+srv://user:encoded_password@cluster0.your_cluster_id.mongodb.net/database_name"
+# for my case above, It looked like
+# mongo "mongodb+srv://admin:p%4055w0rd@cluster0.ufsyo.mongodb.net/test"
+# where my password is 'p@55w0rd', I encoded it on www.url-encode-online.rocks/urlencode
+# post you are able to successfully replace the statement below to point to your cluster.
+
+export MONGO_URL="mongodb+srv://<username>:<encoded_password>@<your_cluster_url>/<database_name>"
+
+
---
+++ prepare-server.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+####################################################
+# DO NOT CHANGE THE GRADLE OPTIONS IN THE BLOCK    #
+# BELOW, IT WILL HAVE IMPACT ON THE PERFORMANCE    #
+# OF YOUR APPLICATION                              #
+####################################################
+GRADLE_OPTS="-Dgradle.user.home=~/gradle_cache"    #
+####################################################
+
+
+
+./gradlew clean bootrun &
+
+while ! netstat -tna | grep 'LISTEN\>' | grep -q ':8081\>'; do
+  echo "waiting for spring application to start"
+  sleep 2 # time in seconds, tune it as needed
+done
+
+# If you have any script to load the data make sure that its part of this bash script.
+
